e87e1bb7f639d874947330dd6501298e9b2e9d9d
nsgabia <norlangabia@gmail.com>
2026-01-14 11:39:48 +0800

    login

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class LoginController extends BaseController
+{
+    public function Sign_in(){
+$db = \Config\Database::connect();
+$builder = $db->table('user'); // Load the Query Builder for 'mytable'
+
+// Build the SELECT query
+$builder->select('username, password');
+$query = $builder->get();
+
+// Get the results
+$results = $query->getResult(); // Returns an array of objects
+// or
+$resultsArray = $query->getResultArray(); // Returns an array of arrays
+
+// Iterate through results
+print_r($results);
+}
+}
