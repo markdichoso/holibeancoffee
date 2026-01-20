ba1e0c8fe31f911b778652d5f9e38e6c170c1442
nsgabia <norlangabia@gmail.com>
2026-01-20 13:13:56 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://hbis.holibeancoffee.com/';
+    public string $baseURL = 'http://localhost/holibeancoffee/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
