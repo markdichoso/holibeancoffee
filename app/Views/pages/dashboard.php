f1d61bf50a7f4016f1103c5438c57534a4a7b089
nsgabia <norlangabia@gmail.com>
2026-01-21 22:24:23 +0800

    Update dashboard.php

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -2,6 +2,6 @@
         echo session()->get('firstname')." ";
         echo session()->get('lastname')."<br />";
         echo anchor('timein', 'Time-In', ['class' => 'btn btn-primary', 'title' => 'Session Destroy'])."<br />";
-        echo anchor('timeout', 'Time-Out', ['class' => 'btn btn-primary', 'title' => 'Session Destroy'])."<br />";
+        echo anchor('timeout', 'Time-Out', ['class' => 'btn btn-primary', 'title' => 'Session Destroy'])."<br /><br />";
         echo anchor('destroy', 'Destroy Session', ['class' => 'btn btn-primary', 'title' => 'Session Destroy']);
 ?>
