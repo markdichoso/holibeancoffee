7a99d8e24913150d4a88cbdecd605880254da9ce
nsgabia <norlangabia@gmail.com>
2026-01-22 13:42:47 +0800

    oryt

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -3,5 +3,5 @@
         echo session()->get('lastname')."</H3><br />";
         echo anchor('timein', 'Time-In', ['class' => 'btn btn-primary', 'title' => 'Time In'])."<br />";
         echo anchor('timeout', 'Time-Out', ['class' => 'btn btn-primary', 'title' => 'Time Out'])."<br /><br />";
-        echo anchor('destroy', 'Destroy Session', ['class' => 'btn btn-primary', 'title' => 'Session Destroy']);
+        echo anchor('destroy', 'Out na Tayo', ['class' => 'btn btn-primary', 'title' => 'Session Destroy']);
 ?>
