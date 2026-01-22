a35bd4928f55d264516a84626aa805db11ac54ab
nsgabia <norlangabia@gmail.com>
2026-01-22 20:39:10 +0800

    12 hour format

--- app/Views/pages/monitoring/timein.php
+++ app/Views/pages/monitoring/timein.php
@@ -362,7 +362,7 @@
         function updateTime() {
             const now = new Date();
             const timeString = now.toLocaleTimeString("en-US", {
-                hour12: false,
+                hour12: true,
                 hour: "2-digit",
                 minute: "2-digit",
                 second: "2-digit",
