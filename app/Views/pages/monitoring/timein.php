ed029d08dea8559c8a36b0c357138a707c8df02e
nsgabia <norlangabia@gmail.com>
2026-08-25 12:20:28 +0800

    Update timein.php

--- app/Views/pages/monitoring/timein.php
+++ app/Views/pages/monitoring/timein.php
@@ -764,9 +764,9 @@
         }
 
         function updateDurationDisplays() {
-            resetDailyData();
-            resetWeeklyData();
-            resetMonthlyData();
+            //resetDailyData();
+            //resetWeeklyData();
+            //resetMonthlyData();
             const now = new Date();
             let currentSessionMinutes = 0;
             if (isTimedIn && currentSessionStart) {
